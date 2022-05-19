79e82265f5f98519e840e235b39c3628df6aa1a1
bszanto111 <62655896+bszanto111@users.noreply.github.com>
2022-05-19 09:25:29 +0200

    Added modern look to login component. Replaced template-driven form with model-driven on login component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
+import { DropdownModule } from 'primeng/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { GameComponent } from './components/game/game.component';
 import { GamePanelComponent } from './components/game-panel/game-panel.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,13 +24,15 @@ import { GamePanelComponent } from './components/game-panel/game-panel.component
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule, 
-    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     InputTextModule,
     CardModule,
     ButtonModule,
-    PasswordModule
+    PasswordModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SelectItem } from 'primeng/api';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
 import { UserService } from 'src/app/services/user.service';
@@ -12,10 +14,14 @@ import { UserService } from 'src/app/services/user.service';
 export class LoginComponent implements OnInit {
 
   users: User[];
-  selectedUserId: string;
-  password: string;
+  userSelectItems: SelectItem<User>[] = [];
   hasAlreadyTriedToLogIn: boolean = false;
   errorMessage: string;
+
+  loginForm: FormGroup;
+  userDropdownControl: FormControl;
+  userIdInputControl: FormControl;
+  passwordControl: FormControl;
 
   constructor(
     private loginService: LoginService,
@@ -23,8 +29,17 @@ export class LoginComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
     this.userService.getUsers().subscribe(
-      users => this.users = users
+      users => {
+        this.users = users;
+        this.userSelectItems = users.map(user => {
+          return {
+            label: user.id + "-" + user.name,
+            value: user
+          }
+        });
+      }
     );
     this.loginService.isLoggedIn$.subscribe(isLoggedIn => {
       if (isLoggedIn) {
@@ -36,12 +51,26 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  selectUser(event, user) {
-    this.selectedUserId = user.id;
+  initializeForm(): void {
+    this.userDropdownControl = new FormControl(null);
+    this.userIdInputControl = new FormControl(null);
+    this.passwordControl = new FormControl(null);
+    this.loginForm = new FormGroup({
+      userDropdown: this.userDropdownControl,
+      userIdInput: this.userIdInputControl,
+      password: this.passwordControl
+    });
+    this.userDropdownControl.valueChanges.subscribe(
+      (user: User) => {
+        if (user) {
+          this.userIdInputControl.setValue(user.id);
+        }
+      }
+    );
   }
 
   login() {
-    this.loginService.login(this.selectedUserId, this.password);
+    this.loginService.login(this.userIdInputControl.value, this.passwordControl.value);
     this.hasAlreadyTriedToLogIn = true;
   }
 }
